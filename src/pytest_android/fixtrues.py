16f88989afb12e6060bb9fa65a66b85bc0426efd
edsion1107 <zjb861107@gmail.com>
2019-01-12 20:42:53 +0800

    修改设备初始化逻辑，支持直接通过Wi-Fi连接设备

--- src/pytest_android/fixtrues.py
+++ src/pytest_android/fixtrues.py
@@ -29,26 +29,31 @@ __all__ = ['driver', 'show_case_name', 'app_start', 'app_stop']
 @pytest.fixture(scope='session', autouse=True)
 def driver(variables: dict) -> UIAutomatorServer:
     """初始化设备
-    解锁、清理后台，设置驱动层参数和执行某些功能插件的注册"""
+    1. 连接设备。(配置文件中)需要指定`device`节点，至少包括`serial`（有线）和`addr`（无线）其中一个。有线连接比无线优先级高；
+不存在`serial`时，直接使用无线连接；通过`serial`连接设备失败时，自动使用`addr`重连；`serial`值为空，自动连接当前唯一的设备
+    2. 设置 uiautomator2 的全局参数，目前有：click_post_delay、wait_timeout
+    3. 执行 uiautomator2 功能插件的注册，目前有：点击、滑动时自动截图
+    """
 
-    assert variables  # 需要通过 pytest 的命令行参数"--variables xxx"指定配置文件
-    logging.info(json.dumps(variables, indent=4))
+    assert variables  # 需要通过 pytest 的命令行参数"--variables xxx"指定配置文件，可以同时指定多个
+    logging.debug(json.dumps(variables, indent=4))
     device = variables.get('device')
+    assert device
 
     logging.debug("设备参数：{0}".format(json.dumps(device, indent=4)))
     u2_conf = variables.get('uiautomator2')
     logging.debug("uiautomator2配置：{0}".format(json.dumps(u2_conf, indent=4)))
     try:
         logging.info('尝试通过USB连接设备')
-        d = uiautomator2.connect_usb(device.get('serial'))
+        if 'serial' not in device:  # 没有 serial 参数直接走Wi-Fi连接
+            raise OSError
+        else:
+            d = uiautomator2.connect_usb(device.get('serial'))
     except OSError:
         logging.warning('尝试通过WI-FI（网络）连接设备')
         d = uiautomator2.connect_wifi(device.get('addr'))
 
     logging.info('device info: {0}'.format(json.dumps(d.device_info, indent=4)))
-
-    d.app_stop_all()  # 清理操作
-    d.watchers.remove()  # 移除 watchers
 
     def _screenshot(stage, func_name, args, kwargs, ret):
         if stage == 'before':
@@ -85,7 +90,7 @@ def driver(variables: dict) -> UIAutomatorServer:
 @pytest.fixture(scope='function', autouse=True)
 def show_case_name(request: SubRequest, driver: UIAutomatorServer) -> None:
     """toast提示显示用例描述或名字，便于了解进度"""
-    # 因为要照顾pytest.mark.parametrize生成的多条case，所以这里用node的name
+    # 因为要支持 pytest.mark.parametrize 生成的多条case的情况，所以这里用node的name
     logging.info(f'case name: {request.node.name}')
     driver.toast.show(request.node.name, 3)  # toast提示显示2秒
 
